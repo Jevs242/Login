3b9b8d65fd2fcc19e4d0c39f3d9d6f7c644a1fee
jevs242 <ghostraidenpm@gmail.com>
2022-08-06 00:56:35 -0400

    CS

---
+++ Login/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Login
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_Email = new System.Windows.Forms.Label();
+            this.tbx_Email = new System.Windows.Forms.TextBox();
+            this.tbx_password = new System.Windows.Forms.TextBox();
+            this.lbl_Password = new System.Windows.Forms.Label();
+            this.lbl_login = new System.Windows.Forms.Label();
+            this.btn_Summit = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lbl_newhere = new System.Windows.Forms.Label();
+            this.btn_signup = new System.Windows.Forms.Button();
+            this.lbl_Error = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbl_Email
+            // 
+            this.lbl_Email.AutoSize = true;
+            this.lbl_Email.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Email.Location = new System.Drawing.Point(27, 148);
+            this.lbl_Email.Name = "lbl_Email";
+            this.lbl_Email.Size = new System.Drawing.Size(89, 31);
+            this.lbl_Email.TabIndex = 0;
+            this.lbl_Email.Text = "Email:";
+            // 
+            // tbx_Email
+            // 
+            this.tbx_Email.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbx_Email.Location = new System.Drawing.Point(112, 155);
+            this.tbx_Email.Name = "tbx_Email";
+            this.tbx_Email.Size = new System.Drawing.Size(318, 22);
+            this.tbx_Email.TabIndex = 1;
+            // 
+            // tbx_password
+            // 
+            this.tbx_password.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbx_password.Location = new System.Drawing.Point(163, 253);
+            this.tbx_password.Name = "tbx_password";
+            this.tbx_password.Size = new System.Drawing.Size(318, 22);
+            this.tbx_password.TabIndex = 2;
+            // 
+            // lbl_Password
+            // 
+            this.lbl_Password.AutoSize = true;
+            this.lbl_Password.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Password.Location = new System.Drawing.Point(21, 246);
+            this.lbl_Password.Name = "lbl_Password";
+            this.lbl_Password.Size = new System.Drawing.Size(142, 31);
+            this.lbl_Password.TabIndex = 3;
+            this.lbl_Password.Text = "Password:";
+            // 
+            // lbl_login
+            // 
+            this.lbl_login.AutoSize = true;
+            this.lbl_login.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_login.Location = new System.Drawing.Point(76, 63);
+            this.lbl_login.Name = "lbl_login";
+            this.lbl_login.Size = new System.Drawing.Size(336, 37);
+            this.lbl_login.TabIndex = 5;
+            this.lbl_login.Text = "Login to Your Account";
+            // 
+            // btn_Summit
+            // 
+            this.btn_Summit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Summit.Location = new System.Drawing.Point(178, 335);
+            this.btn_Summit.Name = "btn_Summit";
+            this.btn_Summit.Size = new System.Drawing.Size(109, 34);
+            this.btn_Summit.TabIndex = 4;
+            this.btn_Summit.Text = "Sign in";
+            this.btn_Summit.UseVisualStyleBackColor = true;
+            this.btn_Summit.Click += new System.EventHandler(this.btn_Summit_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox1.Location = new System.Drawing.Point(510, -2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(290, 454);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lbl_newhere
+            // 
+            this.lbl_newhere.AutoSize = true;
+            this.lbl_newhere.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lbl_newhere.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_newhere.Location = new System.Drawing.Point(595, 157);
+            this.lbl_newhere.Name = "lbl_newhere";
+            this.lbl_newhere.Size = new System.Drawing.Size(150, 31);
+            this.lbl_newhere.TabIndex = 7;
+            this.lbl_newhere.Text = "New Here?";
+            // 
+            // btn_signup
+            // 
+            this.btn_signup.Location = new System.Drawing.Point(625, 208);
+            this.btn_signup.Name = "btn_signup";
+            this.btn_signup.Size = new System.Drawing.Size(86, 30);
+            this.btn_signup.TabIndex = 8;
+            this.btn_signup.Text = "Sign Up";
+            this.btn_signup.UseVisualStyleBackColor = true;
+            // 
+            // lbl_Error
+            // 
+            this.lbl_Error.AutoSize = true;
+            this.lbl_Error.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Error.ForeColor = System.Drawing.Color.Firebrick;
+            this.lbl_Error.Location = new System.Drawing.Point(134, 290);
+            this.lbl_Error.Name = "lbl_Error";
+            this.lbl_Error.Size = new System.Drawing.Size(0, 31);
+            this.lbl_Error.TabIndex = 9;
+            this.lbl_Error.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbl_Error);
+            this.Controls.Add(this.btn_signup);
+            this.Controls.Add(this.lbl_newhere);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lbl_login);
+            this.Controls.Add(this.btn_Summit);
+            this.Controls.Add(this.lbl_Password);
+            this.Controls.Add(this.tbx_password);
+            this.Controls.Add(this.tbx_Email);
+            this.Controls.Add(this.lbl_Email);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_Email;
+        private System.Windows.Forms.TextBox tbx_Email;
+        private System.Windows.Forms.TextBox tbx_password;
+        private System.Windows.Forms.Label lbl_Password;
+        private System.Windows.Forms.Label lbl_login;
+        private System.Windows.Forms.Button btn_Summit;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lbl_newhere;
+        private System.Windows.Forms.Button btn_signup;
+        private System.Windows.Forms.Label lbl_Error;
+    }
+}
+
---
+++ Login/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace Login
+{
+    public partial class Form1 : Form
+    {
+        List<string> listEmail = new List<string>();
+        List<string> listPassword = new List<string>();
+
+        private void readExcel()
+        {
+            string path = Directory.GetCurrentDirectory() + "\\Accounts.xlsx";
+            Excel.Application excel = new Excel.Application();
+            Workbook wb;
+            Worksheet ws;
+
+            wb = excel.Workbooks.Open(path);
+
+            ws = wb.Worksheets[1];
+
+            Range columns = ws.Columns["A:A"];   //Range["A2:A6"]; //[Colums , Row]
+
+            foreach (string Result in columns.Value)
+            {
+                listEmail.Add(Result);
+            }
+
+            columns = ws.Columns["B:B"];
+            foreach (string Result in columns.Value)
+            {
+                listPassword.Add(Result);
+            }
+
+            //MessageBox.Show(cellValue);
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            readExcel();
+        }
+        
+
+        public void OpenFile()
+        {
+           //MessageBox.Show(cell.)
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Summit_Click(object sender, EventArgs e)
+        {
+            string email = tbx_Email.Text;
+            string password = tbx_password.Text;
+            bool success = false; 
+
+            for(int i = 0; i < listEmail.Count; i++)
+            {
+                if(listEmail[i] == "Email")
+                {
+                    continue;
+                }
+
+                if(listEmail[i] == email.ToLower())
+                {
+                    //lbl_Error.Text = "This email address is already used";
+                    success = true;
+                    break;
+                }
+            }
+            if(success)
+            {
+                for (int i = 0; i < listPassword.Count; i++)
+                {
+                    if (listEmail[i] == "Password")
+                    {
+                        continue;
+                    }
+
+                    if (listPassword[i] == password)
+                    {
+                        success = true;
+                        break;
+                    }
+                    else
+                    {
+                        success = false;
+                    }
+                }
+            }
+            if (success)
+            {
+                lbl_Error.Text = "Success to sign in";
+                lbl_Error.ForeColor = Color.DeepSkyBlue;
+            }
+            else
+            {
+                lbl_Error.Text = "Failure to sign in";
+                lbl_Error.ForeColor = Color.Firebrick;
+            }
+
+        }
+    }
+
+    
+}
